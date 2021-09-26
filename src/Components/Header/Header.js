b4de61f15115d1a88ed3e82d159b7596142f276c
Mahir-Asef <mahirasef30@gmail.com>
2021-09-26 21:13:42 +0600

    almost finished

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -6,7 +6,7 @@ import './Header.css'
 const Header = () => {
     return (
         <div className="header-container">
-            <h1>Make A Power Team</h1>
+            <h1>Make A Powerful Team</h1>
             <h4>Hare is some most famous people around the world</h4>
             <h1>Net Worth : 1000 billion</h1>
         </div>
